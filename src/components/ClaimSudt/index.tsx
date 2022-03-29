3d71ddccd17e0d9a25a0fb221f6172ab238258f7
wangximing <sighwang@outlook.com>
2022-03-29 21:29:39 +0800

    refactor: delete unuse code

--- src/components/ClaimSudt/index.tsx
+++ src/components/ClaimSudt/index.tsx
@@ -1,27 +1,12 @@
 import { CkitProvider, MintOptions, MintSudtBuilder, predefined } from "@ckitjs/ckit";
-import { SnakeScript, HashType } from "@lay2/pw-core";
-import { HexString } from "@ckb-lumos/base";
 import { Secp256k1Signer } from "@ckitjs/ckit/dist/wallets/Secp256k1Wallet";
 import { notification } from "antd";
 import { useLightGodwoken } from "../../hooks/useLightGodwoken";
 import Link from "antd/lib/typography/Link";
 import { BI } from "@ckb-lumos/lumos";
-import { CKB_INDEXER_URL, CKB_RPC_URL, CKB_SUDT_SCRIPT_CODE_HASH, CKB_USDC_ISSUER_PRIVATE_KEY } from "../../config";
+import { CKB_INDEXER_URL, CKB_RPC_URL, CKB_USDC_ISSUER_PRIVATE_KEY } from "../../config";
 
-export function randomHexString(lengthWithOut0x: number): HexString {
-  return "0x" + [...Array(lengthWithOut0x)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
-}
-
-export function generateLayer1SUDTTypeScript(args: string): SnakeScript {
-  return {
-    code_hash: CKB_SUDT_SCRIPT_CODE_HASH,
-    hash_type: HashType.type,
-    args,
-  };
-}
-
-export const claim = async (recipientAddr: string) => {
-  console.log(CKB_INDEXER_URL, CKB_RPC_URL);
+const claim = async (recipientAddr: string) => {
   const provider = new CkitProvider(CKB_INDEXER_URL, CKB_RPC_URL);
   await provider.init(predefined.Aggron);
 
@@ -50,7 +35,7 @@ export const ClaimSudt: React.FC = () => {
 
   const claimSudt = async () => {
     if (!lightGodwoken) {
-      throw new Error();
+      throw new Error("LightGodwoken Not Found!");
     }
     const txHash = await claim(lightGodwoken.provider.l1Address);
     notification.success({ message: `deposit Tx(${txHash}) is successful` });
