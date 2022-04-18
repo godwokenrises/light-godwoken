f2699df9614432a99cc974f7f8f94c87284a91d3
wangximing <sighwang@outlook.com>
2022-04-18 19:36:45 +0800

    feat: merge request withdrawal and withdrawal list to one page

--- src/views/WithdrawalV1.tsx
+++ src/views/WithdrawalV1.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { WithdrawalList } from "../components/Withdrawal/List";
+import RequestWithdrawal from "../components/Withdrawal/RequestWithdrawal";
 
 const PageContent = styled.div`
   width: 436px;
@@ -13,38 +14,7 @@ const PageContent = styled.div`
     font-size: 20px;
     font-weight: 600;
     line-height: 1.1;
-    margin-bottom: 8px;
-  }
-  .description {
-    font-weight: 400;
-    line-height: 1.5;
-    font-size: 14px;
-  }
-  .button-container {
-    margin-top: 16px;
-  }
-  .request-button {
-    -webkit-box-align: center;
-    align-items: center;
-    border: 0px;
-    border-radius: 16px;
-    box-shadow: rgb(14 14 44 / 40%) 0px -1px 0px 0px inset;
-    cursor: pointer;
-    display: inline-flex;
-    font-family: inherit;
-    font-size: 16px;
-    font-weight: 600;
-    -webkit-box-pack: center;
-    justify-content: center;
-    letter-spacing: 0.03em;
-    line-height: 1;
-    opacity: 1;
-    outline: 0px;
-    transition: background-color 0.2s ease 0s, opacity 0.2s ease 0s;
-    height: 48px;
-    padding: 0px 24px;
-    background-color: rgb(255, 67, 66);
-    color: white;
+    margin-bottom: 20px;
   }
 `;
 
@@ -64,28 +34,16 @@ const ResultList = styled.div`
   }
 `;
 
-interface Props {
-  onViewChange: (viewName: string) => void;
-}
-
-const WithdrawalV1: React.FC<Props> = ({ onViewChange }) => {
-  const changeActiveView = () => {
-    onViewChange("request-withdrawal");
-  };
-
+const WithdrawalV1: React.FC = () => {
   return (
     <>
       <PageContent className="content">
         <div className="request-withdrawal">
           <div className="title">Withdrawal</div>
-          <div className="button-container">
-            <div onClick={changeActiveView} className="request-button">
-              Request Withdrawal
-            </div>
-          </div>
+          <RequestWithdrawal></RequestWithdrawal>
         </div>
         <ResultList className="withdrawal-request">
-          <div className="header">Your Withdrawal Requests</div>
+          <div className="header">Withdrawal Requests List</div>
           <div className="list">
             <WithdrawalList></WithdrawalList>
           </div>
