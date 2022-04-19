465f9dc0c7b98cc61ab8a8a48838792afe4a96d3
zhangyouxin <zhangyouxingg@126.com>
2022-04-19 15:16:27 +0800

    feat: install @ckb-lumos/codec

--- src/light-godwoken/schemas/codec.ts
+++ src/light-godwoken/schemas/codec.ts
@@ -1,18 +1,7 @@
-import {
-  createObjectCodec,
-  Byte32,
-  Uint64,
-  enhancePack,
-  table,
-  Bytes,
-  struct,
-  Uint32,
-  Uint128,
-  Uint8,
-  option,
-} from "@ckb-lumos/experiment-codec/";
-import { createFixedBytesCodec } from "@ckb-lumos/experiment-codec/lib/base";
-import { BI } from "@ckb-lumos/lumos";
+import { molecule, number, blockchain, createFixedBytesCodec } from "@ckb-lumos/codec/";
+const { table, option, struct } = molecule;
+const { Bytes, Byte32 } = blockchain;
+const { Uint32, Uint128, Uint8, Uint64 } = number;
 
 const hashTypeCodec = createFixedBytesCodec<string>({
   byteLength: 1,
@@ -27,7 +16,7 @@ const hashTypeCodec = createFixedBytesCodec<string>({
     } else {
       throw new Error(`invalid hash type: ${type}`);
     }
-    return data.buffer;
+    return new Uint8Array(data.buffer);
   },
   unpack: (buf) => {
     const data = new DataView(buf).getUint8(0);
@@ -42,62 +31,6 @@ const hashTypeCodec = createFixedBytesCodec<string>({
     }
   },
 });
-const RawWithdrawalLockArgsCodec = createObjectCodec({
-  rollupHash: Byte32,
-  withdrawalBlockhash: Byte32,
-  withdrawalBlockNumber: Uint64,
-  accountScriptHash: Byte32,
-  ownerLockHash: Byte32,
-  // script: table({
-  //   code_hash: Byte32,
-  //   hash_type: Byte32,
-  //   args: Bytes,
-  // },["code_hash", "hash_type", "args"]),
-});
-export const WithdrawalLockArgsCodec = enhancePack(
-  RawWithdrawalLockArgsCodec,
-  () => new ArrayBuffer(0),
-  (buf: ArrayBuffer) => ({
-    rollupHash: buf.slice(0, 32),
-    withdrawalBlockhash: buf.slice(32, 64),
-    withdrawalBlockNumber: buf.slice(64, 72),
-    accountScriptHash: buf.slice(72, 104),
-    ownerLockHash: buf.slice(104, 136),
-    script: buf.slice(136),
-  }),
-);
-
-// struct RawWithdrawalRequest {
-//   nonce: Uint32,
-//   // CKB amount
-//   capacity: Uint64,
-//   // SUDT amount
-//   amount: Uint128,
-//   sudt_script_hash: Byte32,
-//   // layer2 account_script_hash
-//   account_script_hash: Byte32,
-//   // buyer can pay sell_amount and sell_capacity to unlock
-//   sell_amount: Uint128,
-//   sell_capacity: Uint64,
-//   // layer1 lock to withdraw after challenge period
-//   owner_lock_hash: Byte32,
-//   // layer1 lock to receive the payment, must exists on the chain
-//   payment_lock_hash: Byte32,
-//   // withdrawal fee, paid to block producer
-//   fee: Fee,
-// }
-
-// struct Fee {
-//   sudt_id: Uint32,
-//   amount: Uint128,
-// }
-
-// vector WithdrawalRequestVec <WithdrawalRequest>;
-
-// table WithdrawalRequest {
-//   raw: RawWithdrawalRequest,
-//   signature: Bytes,
-// }
 export const RawWithdrwalCodec = struct(
   {
     nonce: Uint32,
