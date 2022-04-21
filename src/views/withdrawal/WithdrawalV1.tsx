8f2ac467003163f80b1485351d971ccf06d249f1
wangximing <sighwang@outlook.com>
2022-04-21 21:29:06 +0800

    feat: show V1 withdrawal transaction history

---
+++ src/views/withdrawal/WithdrawalV1.tsx
@@ -0,0 +1,28 @@
+import { TransactionHistory } from "../../components/TransactionHistory";
+import { WithdrawalList } from "../../components/Withdrawal/List";
+import RequestWithdrawalV1 from "../../components/Withdrawal/RequestWithdrawalV1";
+import { PageContent, ResultList } from "./WithdrawalStyle";
+
+const WithdrawalV1: React.FC = () => {
+  return (
+    <>
+      <PageContent className="content">
+        <div className="header">
+          <div className="title">Withdrawal</div>
+          <TransactionHistory></TransactionHistory>
+        </div>
+        <div className="request-withdrawal">
+          <RequestWithdrawalV1></RequestWithdrawalV1>
+        </div>
+        <ResultList className="withdrawal-request">
+          <div className="header">Withdrawal Requests List</div>
+          <div className="list">
+            <WithdrawalList></WithdrawalList>
+          </div>
+        </ResultList>
+      </PageContent>
+    </>
+  );
+};
+
+export default WithdrawalV1;
