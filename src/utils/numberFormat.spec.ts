82ecd2a478dc2ec6fa1dd0d34e6387081d269f3e
wangximing <sighwang@outlook.com>
2022-05-05 11:57:47 +0800

    fix: number format error while typing decimals

--- src/utils/numberFormat.spec.ts
+++ src/utils/numberFormat.spec.ts
@@ -4,6 +4,7 @@ import { parseUnit } from "./numberFormat";
 describe("parseUnit", () => {
   it("parse number and float to BI", () => {
     expect(parseUnit("10", 2).toString()).toEqual(BI.from(1000).toString());
+    expect(parseUnit("10.", 2).toString()).toEqual(BI.from(1000).toString());
     expect(parseUnit("10.01", 2).toString()).toEqual(BI.from(1001).toString());
   });
 });
--- src/utils/numberFormat.ts
+++ src/utils/numberFormat.ts
@@ -3,9 +3,11 @@ import { BI } from "@ckb-lumos/lumos";
 export const parseUnit = (value: string, unit: number = 0): BI => {
   const valueArray = value.split(".");
   if (valueArray.length === 2) {
-    return BI.from(valueArray[0])
-      .mul(BI.from(10).pow(unit))
-      .add(BI.from(valueArray[1]).mul(BI.from(10).pow(unit - valueArray[1].length)));
+    let result = BI.from(valueArray[0]).mul(BI.from(10).pow(unit));
+    if (valueArray[1] !== "") {
+      result = result.add(BI.from(valueArray[1]).mul(BI.from(10).pow(unit - valueArray[1].length)));
+    }
+    return result;
   } else if (valueArray.length === 1) {
     return BI.from(value).mul(BI.from(10).pow(unit));
   } else {
