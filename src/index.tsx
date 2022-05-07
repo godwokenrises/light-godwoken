f067cffe6dad6fa62386ec29045dfa23b1788249
zhangyouxin <zhangyouxingg@126.com>
2022-05-07 18:16:16 +0800

    chore: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,21 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { config } from "@ckb-lumos/lumos";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+Sentry.init({
+  environment: process.env.NODE_ENV,
+  release: "light-godwoken@" + "0.1.0",
+  debug: false,
+  dsn: "https://cee76b0263164a9b89c918b194cedbf8@o1235895.ingest.sentry.io/6385985",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 config.initializeConfig(config.predefined.AGGRON4);
 
 ReactDOM.render(
--- src/light-godwoken/debug.ts
+++ src/light-godwoken/debug.ts
@@ -1,1 +1,11 @@
-export const debug = console.debug;
+import * as Sentry from "@sentry/react";
+
+export const debug = (...args: any) => {
+  console.debug(...args);
+  try {
+    const message = args.reduce((acc: string, curr: any) => acc + JSON.stringify(curr) + ", ", "");
+    Sentry.captureMessage(message);
+  } catch (error) {
+    Sentry.captureMessage(args);
+  }
+};
