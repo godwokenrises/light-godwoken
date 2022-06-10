2bfab7725cb41e6f953dd2fb39ea9c7629b63cd9
bing <tarvos21@gmail.com>
2022-06-10 10:32:36 +0800

    add commit hash to release name and update sentry.nervos.org dsn URL

--- src/index.tsx
+++ src/index.tsx
@@ -11,12 +11,14 @@ import { config } from "@ckb-lumos/lumos";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 import { LightGodwokenError } from "./light-godwoken/constants/error";
+
 if (process.env.NODE_ENV === "production" || true) {
   Sentry.init({
     environment: process.env.NODE_ENV,
-    release: "light-godwoken@" + process.env.REACT_APP_VERSION,
+    release: "light-godwoken@" + process.env.REACT_APP_VERSION + "@" + process.env.REACT_APP_COMMIT_HASH,
     debug: false,
-    dsn: "https://cee76b0263164a9b89c918b194cedbf8@o1235895.ingest.sentry.io/6385985",
+    // dsn: "https://cee76b0263164a9b89c918b194cedbf8@o1235895.ingest.sentry.io/6385985",
+    dsn: "https://4f42ad25553a4472bc6f64a8ed2c432c@sentry.nervos.org/17",
     integrations: [new BrowserTracing()],
 
     // Set tracesSampleRate to 1.0 to capture 100%
