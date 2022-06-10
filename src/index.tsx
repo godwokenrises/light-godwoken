8055420e74991cd2ffc83cfb36c058732ea27e65
bing <tarvos21@gmail.com>
2022-06-11 02:28:49 +0800

    update env and config

--- src/index.tsx
+++ src/index.tsx
@@ -12,24 +12,26 @@ import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 import { LightGodwokenError } from "./light-godwoken/constants/error";
 
-if (process.env.NODE_ENV === "production" || true) {
+if (process.env.NODE_ENV === "production") {
   Sentry.init({
     environment: process.env.NODE_ENV,
     release: "light-godwoken@" + process.env.REACT_APP_VERSION + "@" + process.env.REACT_APP_COMMIT_HASH,
     debug: false,
     // dsn: "https://cee76b0263164a9b89c918b194cedbf8@o1235895.ingest.sentry.io/6385985",
-    dsn: "https://4f42ad25553a4472bc6f64a8ed2c432c@sentry.nervos.org/17",
+    // dsn: "https://4f42ad25553a4472bc6f64a8ed2c432c@sentry.nervos.org/17",
+    dsn: process.env.REACT_APP_SENTRY_DSN,
     integrations: [new BrowserTracing()],
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
-    // allowUrls: [
-    //   /https?:\/\/testnet\.bridge\.godwoken\.io/,
-    //   /https?:\/\/light-godwoken\.vercel\.app/,
-    //   /https?:\/\/light-godwoken-mainnet\.vercel\.app/,
-    // ],
+    allowUrls: [
+      /http?:\/\/localhost/,
+      /https?:\/\/testnet\.bridge\.godwoken\.io/,
+      /https?:\/\/light-godwoken\.vercel\.app/,
+      /https?:\/\/light-godwoken-mainnet\.vercel\.app/,
+    ],
     beforeSend: function (event, hint) {
       const exception = hint?.originalException;
       if (exception instanceof LightGodwokenError) {
