1005b372626696be4049934b23de23f248f31697
ShookLyngs <shooklyngs@gmail.com>
2022-09-20 16:44:38 +0800

    fix: token logo for "YOK" and "TAI"

--- packages/light-godwoken/src/tokens/tokens.ts
+++ packages/light-godwoken/src/tokens/tokens.ts
@@ -1,11 +1,8 @@
-import { GodwokenNetwork, GodwokenVersion, LightGodwokenConfig } from "../config";
+import { GodwokenNetwork, GodwokenVersion } from "../config";
 import { LightGodwokenToken } from "./constants";
 import { predefinedTokens } from "./predefined";
 
-export function getTokenList(
-  network: GodwokenNetwork | string,
-  version: GodwokenVersion,
-): LightGodwokenToken[] {
+export function getTokenList(network: GodwokenNetwork | string, version: GodwokenVersion): LightGodwokenToken[] {
   if (!(network in predefinedTokens)) return [];
 
   const listMap = predefinedTokens[network as GodwokenNetwork];
