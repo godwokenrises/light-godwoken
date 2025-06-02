dc98c20264242e2b51a908795eec0bc9b65c995d
Shook <shooklyngs@gmail.com>
2025-06-03 01:06:32 +0800

    feat(godwoken-bridge): add announcement to ban bridge features temporary

--- apps/godwoken-bridge/src/App.tsx
+++ apps/godwoken-bridge/src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Provider as LightGodwokenProvider } from "./contexts/LightGodwokenContext";
-import GodwokenBridge from "./views/GodwokenBridge";
-import Deposit from "./views/Deposit";
-import Withdrawal from "./views/withdrawal/Withdrawal";
-import L1Transfer from "./views/L1Transfer";
-import { ShowOnPhaseOut } from "./components/PhaseOut/ShowOnPhaseOut";
+// import GodwokenBridge from "./views/GodwokenBridge";
+// import Deposit from "./views/Deposit";
+// import Withdrawal from "./views/withdrawal/Withdrawal";
+// import L1Transfer from "./views/L1Transfer";
+// import { ShowOnPhaseOut } from "./components/PhaseOut/ShowOnPhaseOut";
+import { PageSuspended } from "./components/Layout/PageSuspended";
 
 function App() {
   const queryClient = new QueryClient();
@@ -15,7 +16,9 @@ function App() {
       <Router>
         <LightGodwokenProvider>
           <Routes>
-            <Route path="/" element={<Navigate to="v1" />} />
+            <Route  path="/" element={<PageSuspended />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            {/*<Route path="/" element={<Navigate to="v1" />} />
             <Route path=":version/*" element={<GodwokenBridge />}>
               <Route
                 index
@@ -33,7 +36,7 @@ function App() {
                 <Route index element={<Navigate to="pending" />} />
                 <Route path=":status" element={<L1Transfer />} />
               </Route>
-            </Route>
+            </Route>*/}
           </Routes>
         </LightGodwokenProvider>
       </Router>
